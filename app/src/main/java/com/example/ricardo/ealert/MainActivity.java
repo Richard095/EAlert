231731aff6bfcb72f7823c1cfd1a4da8c8b055eb
Richard095 <ricardorm789@gmail.com>
2018-08-17 20:07:01 -0500

    Tercera subida

--- app/src/main/java/com/example/ricardo/ealert/MainActivity.java
+++ app/src/main/java/com/example/ricardo/ealert/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.ricardo.ealert;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView earthquake_list_view = (ListView) findViewById(R.id.earthquake_list_view);
+        ArrayList<String> countriesList = new ArrayList<>();
+
+        countriesList.add("Mexico");
+        countriesList.add("España");
+        countriesList.add("Alemania");
+        countriesList.add("EUA");
+        countriesList.add("Inglaterra");
+
+        ArrayAdapter<String> countriesAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,countriesList);
+
+        earthquake_list_view.setAdapter(countriesAdapter);
     }
 }
